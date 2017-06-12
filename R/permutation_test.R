afb47c85442f74eed8e482f0c82cb08ea55fbdaf
Ilenia Lovato <ilenia.lovato01@universitadipavia.it>
2017-06-12 16:44:27 +0200

    I added the compute of Btol if the user gives in input the tolerance instead of the number of permutation. I added the case in which all the possible permutations are performed

--- R/permutation_test.R
+++ R/permutation_test.R
@@ -15,34 +15,68 @@
 #'   It can be: "adjacency", "laplacian" and "modularity".
 #' @param distance The chosen distance for calculate the test statistic. It can
 #'   be: "hamming", "frobenius", "spectral", "rooteuclidean".
-#' @param statistic The chosen test statistic. It can be: "average" or "frechet".
+#' @param statistic The chosen test statistic. It can be: "average" or
+#'   "frechet".
 #' @param B The number of permutation or the tolerance.
+#' @param modality The parameter for fix the number of permutations, either "b" for
+#'   the exact number of permutations to be performed or "tol" for the
+#'   tolerance.
 #' @param alpha The level of significancy.
 #'
 #' @return \code{get_pvalue} returns a scalar.
 #' @export
 #'
 #' @examples
-get_pvalue <- function(x, y, representation, distance, statistic, B, alpha) {
-  n <- length(x)
-  m <- length(y)
-  N <- n + m
+get_pvalue <- function(x, y, representation, distance, statistic, B, modality, alpha) {
+  n1 <- length(x)
+  n2 <- length(y)
+  n <- n1 + n2
   d <- get_paired_distances(x, y, representation, distance)
 
 
-  #DA FARE: caso con tolleranza e verifica della raggiungibilità di alpha
+  #DA FARE: verifica della raggiungibilità di alpha
 
   T0 <- switch(statistic,
-              average <- get_average_statistic(d, 1:n),
-              frechet <- get_frechet_statistic(d, 1:n))
+              average <- get_average_statistic(d, 1:n1),
+              frechet <- get_frechet_statistic(d, 1:n1))
 
-  Tp <- rep(-1, B)
-  for (i in 1:B) {
-    p <- sample(1:N, N, replace=FALSE)
-    Tp[i] <- switch(statistic,
-                    average <- get_average_statistic(d, p[1:n]),
-                    frechet <- get_frechet_statistic(d, p[1:n]))
+
+  all <- FALSE
+
+  Bnew <- switch(modality,
+                 b <- B,
+                 tol <- (qnorm(alpha/2, lower.tail = FALSE)/tol)^2)
+
+  group1.perm <- t(combn(1:n,n1))
+  M <- dim(group1.perm)[1]
+
+  if (Bnew>=M) {
+    all <- TRUE
   }
+
+
+  if (all) {
+    Tp <- rep(-1, M)
+    for (i in 1:M) {
+      p <- group1.perm[i,]
+      Tp[i] <- switch(statistic,
+                      average <- get_average_statistic(d, p),
+                      frechet <- get_frechet_statistic(d, p))
+    }
+  }
+
+  else {
+    Tp <- rep(-1, Bnew)
+    for (i in 1:B) {
+      p <- sample(1:n, n, replace=FALSE)
+      Tp[i] <- switch(statistic,
+                      average <- get_average_statistic(d, p[1:n1]),
+                      frechet <- get_frechet_statistic(d, p[1:n1]))
+    }
+  }
+
+
+
 
   p <- sum(Tp >= T0)/B
 
