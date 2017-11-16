d0786a30de9b5c02351dda041a14e77aac2e0199
Aymeric Stamm <aymeric@stamm.fr>
2017-11-16 21:34:17 +0100

    Added option to perform exact test.

--- R/permutation_test.R
+++ R/permutation_test.R
@@ -26,11 +26,13 @@
 #'   test statistic, among: \code{"hamming"} [default], \code{"frobenius"},
 #'   \code{"spectral"} and \code{"root-euclidean"}.
 #' @param statistic A string specifying the chosen test statistic, among:
-#'   \code{"average"} [default] or \code{"frechet"}.
+#'   \code{"mod"} [default], \code{"dom"} or \code{"sdom"}.
 #' @param B The number of permutation or the tolerance (default: \code{1000L}).
 #'   If this number is lower than \code{1}, it is intended as a tolerance.
 #'   Otherwise, it is intended as the number of required permutations.
 #' @param alpha The significance level (default: \code{0.05}).
+#' @param verbose A boolean to switch on/off verbosity (information about
+#'   p-value resolution).
 #'
 #' @return A \code{\link[base]{list}} with two components: the value of the
 #'   statistic for the original two samples and the p-value of the resulting
@@ -58,53 +60,59 @@
 #'
 #' test2 <- network_test2p(x, y, "modularity")
 #' test2
-network_test2p <- function(x, y, representation = "adjacency", distance = "hamming",
-                           statistic = "average", B = 1000L, alpha = 0.05) {
+network_test2p <- function(
+  x, y,
+  representation = "adjacency", distance = "hamming", statistic = "mod",
+  B = 1000L, alpha = 0.05, test = "exact", verbose = TRUE) {
   n1 <- length(x)
   n2 <- length(y)
   n <- n1 + n2
 
+  representation <- match.arg(representation, c("adjacency", "laplacian", "modularity"))
+  distance <- match.arg(distance, c("hamming", "frobenius", "spectral", "root-euclidean"))
   d <- get_distance_matrix(x, y, representation, distance)
 
+  statistic <- match.arg(statistic, c("mod", "dom", "sdom"))
   T0 <- switch(
     statistic,
-    average = get_average_statistic(d, 1:n1),
-    frechet = get_frechet_statistic(d, 1:n1)
+    mod = get_mod_statistic(d, 1:n1),
+    dom = get_dom_statistic(d, 1:n1),
+    sdom = get_sdom_statistic(d, 1:n1)
   )
 
   if (B < 1)
     B <- (qnorm(alpha / 2, lower.tail = FALSE) / tol)^2
 
-  if (!reachable_significance(n1, n2, B, alpha, TRUE))
+  if (!reachable_significance(n1, n2, B, alpha, verbose))
     warning("The requested significance level cannot be
             reached given the sample sizes.")
 
   M <- choose(n, n1)
+  if (n1 == n2)
+    M <- M / 2
 
-  if (B >= M) {
-    group1.perm <- t(combn(1:n, n1))
-    Tp <- rep(-1, M)
-    for (i in 1:M) {
-      p <- group1.perm[i, ]
-      Tp[i] <- switch(
-        statistic,
-        average = get_average_statistic(d, p),
-        frechet = get_frechet_statistic(d, p)
-      )
-    }
-  } else {
-    Tp <- rep(-1, B)
-    for (i in 1:B) {
-      p <- sample(1:n, n, replace = FALSE)
-      Tp[i] <- switch(
-        statistic,
-        average = get_average_statistic(d, p[1:n1]),
-        frechet = get_frechet_statistic(d, p[1:n1])
-      )
-    }
+  test <- match.arg(test, c("approximate", "exact"))
+  if (test == "approximate" & M <= B) {
+    B <- M
+    group1.perm <- combn(n, n1)[, 1:B]
+  } else
+    group1.perm <- replicate(B, sample.int(n))[1:n1, ]
+
+  Tp <- switch(
+    statistic,
+    mod = purrr::map_dbl(1:B, ~ get_mod_statistic(d, group1.perm[, .])),
+    dom = purrr::map_dbl(1:B, ~ get_dom_statistic(d, group1.perm[, .])),
+    sdom = purrr::map_dbl(1:B, ~ get_sdom_statistic(d, group1.perm[, .]))
+  )
+
+  if (test == "approximate")
+    p <- mean(Tp >= T0)
+  else {
+    b <- sum(Tp >= T0)
+    # corr <- integrate(pbinom, 0, 0.5 / M, q = b, size = B)$value
+    # p <- (b + 1) / (B + 1) - corr
+    p <- statmod::permp(b, B, n1, n2)
   }
 
-  p <- mean(Tp >= T0)
-
-  list(statistic = T0, pvalue = p)
+  list(statistic = T0, pvalue = p, permuted_statistics = Tp)
 }
