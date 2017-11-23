c996c640ac26a7810d3619c146572dcdd2dd81db
Aymeric Stamm <aymeric@stamm.fr>
2017-11-23 16:38:04 +0100

    Update scenario B to use igraph stochastic block model. Add scenario E as in scenario A but with bigger differences in lambda parameter. Should unravel properties of modularity matrix.

--- R/simulations.R
+++ R/simulations.R
@@ -6,24 +6,6 @@ rpois_network <- function(lambda, n) {
   A + t(A)
 }
 
-rblocks_network <- function(pois_indices, n, upper = TRUE) {
-  m <- n * (n - 1L) / 2L
-  p <- length(pois_indices)
-  if (p > m)
-    stop("Too many indices.")
-
-  weights <- numeric(m)
-  weights[pois_indices] <- rpois(p, 5)
-  weights[-pois_indices] <- rbinom(m - p, 1L, 0.2)
-
-  A <- diag(0, n)
-  if (upper)
-    A[upper.tri(A)] <- weights
-  else
-    A[lower.tri(A)] <- weights
-  A + t(A)
-}
-
 # Scenario 0 --------------------------------------------------------------
 
 get_scenario0_dataset <- function(n1, n2 = n1) {
@@ -45,9 +27,10 @@ get_scenarioA_dataset <- function(n1, n2 = n1) {
 # Scenario B --------------------------------------------------------------
 
 get_scenarioB_dataset <- function(n1, n2 = n1) {
-  n <- 25L
-  x <- replicate(n1, rblocks_network(1:66, n, upper = TRUE), simplify = FALSE)
-  y <- replicate(n2, rblocks_network(222+1:78, n, upper = FALSE), simplify = FALSE)
+  p1 <- matrix(data = c(0.8, 0.2, 0.2, 0.2), nrow = 2L, ncol = 2L)
+  p2 <- matrix(data = c(0.2, 0.2, 0.2, 0.8), nrow = 2L, ncol = 2L)
+  x <- replicate(n1, igraph::sample_sbm(n = 25L, pref.matrix = p1, block.sizes = c(12L, 13L)), simplify = FALSE)
+  y <- replicate(n2, igraph::sample_sbm(n = 25L, pref.matrix = p2, block.sizes = c(13L, 12L)), simplify = FALSE)
   list(x = x, y = y)
 }
 
@@ -55,15 +38,24 @@ get_scenarioB_dataset <- function(n1, n2 = n1) {
 
 get_scenarioC_dataset <- function(n1, n2 = n1) {
   x <- replicate(n1, igraph::sample_k_regular(no.of.nodes = 25L, k = 8L), simplify = FALSE)
-  y <- replicate(n2, igraph::erdos.renyi.game(n = 25L, p.or.m = 1/3, type = "gnp"), simplify = FALSE)
+  y <- replicate(n2, igraph::sample_gnp(n = 25L, p = 1/3), simplify = FALSE)
   list(x = x, y = y)
 }
 
 # Scenario D --------------------------------------------------------------
 
 get_scenarioD_dataset <- function(n1, n2 = n1) {
-  x <- replicate(n1, igraph::watts.strogatz.game(dim = 1L, size = 25L, nei = 4L, p = 0.15), simplify = FALSE)
-  y <- replicate(n2, igraph::barabasi.game(n = 25L, power = 2L, m = 4L, directed = FALSE), simplify = FALSE)
+  x <- replicate(n1, igraph::sample_smallworld(dim = 1L, size = 25L, nei = 4L, p = 0.15), simplify = FALSE)
+  y <- replicate(n2, igraph::sample_pa(n = 25L, power = 2L, m = 4L, directed = FALSE), simplify = FALSE)
+  list(x = x, y = y)
+}
+
+# Scenario E --------------------------------------------------------------
+
+get_scenarioE_dataset <- function(n1, n2 = n1) {
+  n <- 25L
+  x <- replicate(n1, rpois_network(5, n), simplify = FALSE)
+  y <- replicate(n2, rpois_network(20, n), simplify = FALSE)
   list(x = x, y = y)
 }
 
@@ -76,7 +68,8 @@ perform_single_test <- function(scenario, n_pop, representation, distance, stati
     "A" = get_scenarioA_dataset(n_pop),
     "B" = get_scenarioB_dataset(n_pop),
     "C" = get_scenarioC_dataset(n_pop),
-    "D" = get_scenarioD_dataset(n_pop)
+    "D" = get_scenarioD_dataset(n_pop),
+    "E" = get_scenarioE_dataset(n_pop)
   )
 
   test <- network_test2p(
