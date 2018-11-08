506e4e3c31b551150edb345bc034a880a98b9bd2
Aymeric Stamm <aymeric@stamm.fr>
2018-11-08 19:55:08 +0100

    Remove inexisting V global variable. Added namespace utils for combn function.

--- R/possibly_significant_areas.R
+++ R/possibly_significant_areas.R
@@ -19,12 +19,12 @@ possibly_significant_areas_tot <-
       xx <- lapply(x,
                    select_nodes_tot,
                    subsets = Y[[j]],
-                   vertex_set = V)
+                   vertex_set = vertex_set)
       yy <-
         lapply(y,
                select_nodes_tot,
                subsets = Y[[j]],
-               vertex_set = V)
+               vertex_set = vertex_set)
 
       pvalue[j] <- test2_global(
         xx,
@@ -55,12 +55,12 @@ possibly_significant_areas_tot <-
     if (length(nonSign) > 0) {
       if (dimens > 1) {
         previously_excluded <-
-          which(prodlim::row.match(data.frame(t(combn(
+          which(prodlim::row.match(data.frame(t(utils::combn(
             1:N, (dimens + 1)
           ))), data.frame(t(Y)), nomatch = 0) == 0)
         if (length(previously_excluded) > 0) {
           to_be_excluded <-
-            subgroups(combn(1:N, (dimens + 1))[, previously_excluded], dimens)
+            subgroups(utils::combn(1:N, (dimens + 1))[, previously_excluded], dimens)
           eliminate1 <-
             which(prodlim::row.match(data.frame(t(
               subgroups(Y[, which(pvalue < alpha)], dimens)
@@ -86,12 +86,12 @@ possibly_significant_areas_tot <-
 
       if (dimens == 1) {
         previously_excluded <-
-          which(prodlim::row.match(data.frame(t(combn(
+          which(prodlim::row.match(data.frame(t(utils::combn(
             1:N, (dimens + 1)
           ))), data.frame(t(Y)), nomatch = 0) == 0)
         if (length(previously_excluded) > 0) {
           to_be_excluded <-
-            subgroups(combn(1:N, (dimens + 1))[, previously_excluded], dimens)
+            subgroups(utils::combn(1:N, (dimens + 1))[, previously_excluded], dimens)
           eliminate1 <-
             which(prodlim::row.match(
               data.frame(subgroups(Y[, which(pvalue < alpha)], dimens)),
@@ -132,12 +132,12 @@ possibly_significant_areas_tot <-
 
     if (dim(nonSign)[2] == 0) {
       previously_excluded <-
-        which(prodlim::row.match(data.frame(t(combn(
+        which(prodlim::row.match(data.frame(t(utils::combn(
           1:N, (dimens + 1)
         ))), data.frame(t(Y)), nomatch = 0) == 0)
       if (length(previously_excluded) > 0) {
         to_be_excluded <-
-          subgroups(combn(1:N, (dimens + 1))[, previously_excluded], dimens)
+          subgroups(utils::combn(1:N, (dimens + 1))[, previously_excluded], dimens)
         eliminate <-
           which(prodlim::row.match(data.frame(t(
             subgroups(Y[, which(pvalue < alpha)], dimens)
@@ -182,7 +182,7 @@ possibly_significant_areas_int <- function(x, y, Y, V, dimens, p_int, dimens1, r
   #t <- 1
   #for (j in 1:m) {
    # if (pvalue[j] < 0.05) {
-    #  Z[[t]] <- combn(Y[,j], dimens)
+    #  Z[[t]] <- utils::combn(Y[,j], dimens)
      # t <- t + 1
     #}
   #}
@@ -200,9 +200,9 @@ possibly_significant_areas_int <- function(x, y, Y, V, dimens, p_int, dimens1, r
     nonSign <- as.matrix(Y[,which(pvalue>=alpha)])
     if (dim(nonSign)[2] > 0) {
       if (dimens > 1) {
-        previously_excluded <- which(prodlim::row.match(data.frame(t(combn(1:N, (dimens+1)))), data.frame(t(Y)), nomatch = 0)==0)
+        previously_excluded <- which(prodlim::row.match(data.frame(t(utils::combn(1:N, (dimens+1)))), data.frame(t(Y)), nomatch = 0)==0)
         if (length(previously_excluded)>0) {
-          to_be_excluded <- subgroups(combn(1:N, (dimens+1))[, previously_excluded], dimens)
+          to_be_excluded <- subgroups(utils::combn(1:N, (dimens+1))[, previously_excluded], dimens)
           eliminate1 <- which(prodlim::row.match(data.frame(t(subgroups(Y[,which(pvalue<alpha)], dimens))), data.frame(t(to_be_excluded)), nomatch = 0)>0)
           eliminate2 <- which(prodlim::row.match(data.frame(t(subgroups(Y[,which(pvalue<alpha)], dimens))), data.frame(t(subgroups(Y[,which(pvalue>=alpha)], dimens))), nomatch = 0)>0)
           eliminate <- unique(sort(c(eliminate1, eliminate2)))
@@ -212,9 +212,9 @@ possibly_significant_areas_int <- function(x, y, Y, V, dimens, p_int, dimens1, r
       }
       #eliminate <- which(prodlim::row.match(data.frame(t(subgroups(Y[,which(pvalue<alpha)], dimens))), data.frame(t(subgroups(Y[,which(pvalue>=alpha)], dimens))), nomatch = 0)>0)
       if (dimens == 1) {
-        previously_excluded <- which(prodlim::row.match(data.frame(t(combn(1:N, (dimens+1)))), data.frame(t(Y)), nomatch = 0)==0)
+        previously_excluded <- which(prodlim::row.match(data.frame(t(utils::combn(1:N, (dimens+1)))), data.frame(t(Y)), nomatch = 0)==0)
         if (length(previously_excluded)>0) {
-          to_be_excluded <- subgroups(combn(1:N, (dimens+1))[, previously_excluded], dimens)
+          to_be_excluded <- subgroups(utils::combn(1:N, (dimens+1))[, previously_excluded], dimens)
           eliminate1 <- which(prodlim::row.match(data.frame(subgroups(Y[,which(pvalue<alpha)], dimens)), data.frame(to_be_excluded), nomatch = 0)>0)
           eliminate2 <- which(prodlim::row.match(data.frame(subgroups(Y[,which(pvalue<alpha)], dimens)), data.frame(subgroups(Y[,which(pvalue>=alpha)], dimens)), nomatch = 0)>0)
           eliminate <- unique(sort(c(eliminate1, eliminate2)))
@@ -236,9 +236,9 @@ possibly_significant_areas_int <- function(x, y, Y, V, dimens, p_int, dimens1, r
     }
 
     if (dim(nonSign)[2] == 0) {
-      previously_excluded <- which(prodlim::row.match(data.frame(t(combn(1:N, (dimens+1)))), data.frame(t(Y)), nomatch = 0)==0)
+      previously_excluded <- which(prodlim::row.match(data.frame(t(utils::combn(1:N, (dimens+1)))), data.frame(t(Y)), nomatch = 0)==0)
       if (length(previously_excluded)>0) {
-        to_be_excluded <- subgroups(combn(1:N, (dimens+1))[, previously_excluded], dimens)
+        to_be_excluded <- subgroups(utils::combn(1:N, (dimens+1))[, previously_excluded], dimens)
         eliminate <- which(prodlim::row.match(data.frame(t(subgroups(Y[,which(pvalue<alpha)], dimens))), data.frame(t(to_be_excluded)), nomatch = 0)>0)
 
         if (length(eliminate) == dim(subgroups(Y[,which(pvalue<alpha)], dimens))[2]) {
@@ -254,7 +254,7 @@ possibly_significant_areas_int <- function(x, y, Y, V, dimens, p_int, dimens1, r
     }
 
     if (dimens <= length(dimens1)) {
-      no_int <- combn(dimens1, dimens)
+      no_int <- utils::combn(dimens1, dimens)
       eliminate_int <- which(prodlim::row.match(data.frame(t(c)), data.frame(t(no_int)), nomatch = 0)>0)
       if (length(eliminate_int)>0) {
         if (length(eliminate_int) == dim(c)[2]) {
@@ -270,7 +270,7 @@ possibly_significant_areas_int <- function(x, y, Y, V, dimens, p_int, dimens1, r
     }
 
     possibly_significant_areas <- c
-    #possibly_significant_areas <- combn(Z, dimens)
+    #possibly_significant_areas <- utils::combn(Z, dimens)
     results <- list(exit_int, possibly_significant_areas, p_int)
     return(results)
   }
@@ -288,7 +288,7 @@ possibly_significant_areas_ext <- function(x, y, Y, V, dimens, p_ext, representa
 
     set.seed(1234)
     pvalue[j] <- test_twosample(xx, yy, statistic = c("sot", "lot"), representation = representation, distance = distance, alpha = alpha, B = B)[[2]]
-    inter <- combn(sort(Y[,j]),2)
+    inter <- utils::combn(sort(Y[,j]),2)
     mm <- dim(inter)[2]
     for (k in 1:mm) {
       p_ext[inter[1,k], inter[2,k]] <- max(as.numeric(p_ext[inter[1,k], inter[2,k]]), as.numeric(pvalue[j]))
@@ -308,13 +308,13 @@ possibly_significant_areas_ext <- function(x, y, Y, V, dimens, p_ext, representa
     exit_ext <- FALSE
     nonSign <- as.matrix(Y[,which(pvalue>=alpha)])
     if (dim(nonSign)[2] > 0) {
-      #c <- cbind(combn(Z,dimens),combn(Y[,which(pvalue>=0.05)],dimens))
+      #c <- cbind(utils::combn(Z,dimens),utils::combn(Y[,which(pvalue>=0.05)],dimens))
       #c <- c[,!duplicated(c, MARGIN = 2)]
       #aggiungere ciclio?
 
-      previously_excluded <- which(prodlim::row.match(data.frame(t(combn(1:N, (dimens+1)))), data.frame(t(Y)), nomatch = 0)==0)
+      previously_excluded <- which(prodlim::row.match(data.frame(t(utils::combn(1:N, (dimens+1)))), data.frame(t(Y)), nomatch = 0)==0)
       if (length(previously_excluded)>0) {
-        to_be_excluded <- subgroups(combn(1:N, (dimens+1))[, previously_excluded], dimens)
+        to_be_excluded <- subgroups(utils::combn(1:N, (dimens+1))[, previously_excluded], dimens)
         eliminate1 <- which(prodlim::row.match(data.frame(t(subgroups(Y[,which(pvalue<alpha)], dimens))), data.frame(t(to_be_excluded)), nomatch = 0)>0)
         eliminate2 <- which(prodlim::row.match(data.frame(t(subgroups(Y[,which(pvalue<alpha)], dimens))), data.frame(t(subgroups(Y[,which(pvalue>=alpha)], dimens))), nomatch = 0)>0)
         eliminate <- unique(sort(c(eliminate1, eliminate2)))
@@ -335,9 +335,9 @@ possibly_significant_areas_ext <- function(x, y, Y, V, dimens, p_ext, representa
     }
 
     if (dim(nonSign)[2] == 0) {
-      previously_excluded <- which(prodlim::row.match(data.frame(t(combn(1:N, (dimens+1)))), data.frame(t(Y)), nomatch = 0)==0)
+      previously_excluded <- which(prodlim::row.match(data.frame(t(utils::combn(1:N, (dimens+1)))), data.frame(t(Y)), nomatch = 0)==0)
       if (length(previously_excluded)>0) {
-        to_be_excluded <- subgroups(combn(1:N, (dimens+1))[, previously_excluded], dimens)
+        to_be_excluded <- subgroups(utils::combn(1:N, (dimens+1))[, previously_excluded], dimens)
         eliminate <- which(prodlim::row.match(data.frame(t(subgroups(Y[,which(pvalue<alpha)], dimens))), data.frame(t(to_be_excluded)), nomatch = 0)>0)
 
         if (length(eliminate) == dim(subgroups(Y[,which(pvalue<alpha)], dimens))[2]) {
@@ -354,7 +354,7 @@ possibly_significant_areas_ext <- function(x, y, Y, V, dimens, p_ext, representa
 
 
     possibly_significant_areas <- c
-    #possibly_significant_areas <- combn(Z, dimens)
+    #possibly_significant_areas <- utils::combn(Z, dimens)
     results <- list(exit_ext, possibly_significant_areas, p_ext)
     return(results)
   }
