1bac3bc786b6201a02cbbe54967c3a9cabc7d71d
Aymeric Stamm <aymeric@stamm.fr>
2018-11-14 10:24:44 +0100

    Remove function for computing all three subgraphs simultaneously.

--- R/subgraphs.R
+++ R/subgraphs.R
@@ -14,7 +14,6 @@
 #' g_full  <- subgraph_full (g, list(1:3, 4:5, 8:10))
 #' g_intra <- subgraph_intra(g, list(1:3, 4:5, 8:10))
 #' g_inter <- subgraph_inter(g, list(1:3, 4:5, 8:10))
-#' g_all   <- subgraph_all  (g, list(1:3, 4:5, 8:10))
 #'
 #' @name subgraphs
 NULL
@@ -53,26 +52,6 @@ subgraph_inter <- function(g, vids) {
     purrr::reduce(igraph::disjoint_union) %>%
     igraph::difference(big = subgraph_full(g, vids)) %>%
     igraph::set_graph_attr("atoms", names(vids))
-}
-
-#' @export
-#' @rdname subgraphs
-subgraph_all <- function(g, vids) {
-  if (!igraph::is_named(g)) igraph::V(g)$name <- seq_len(igraph::vcount(g))
-  # Handle graph attributes
-  ga_names <- igraph::graph_attr_names(g)
-  ga_values <- ga_names %>%
-    purrr::map(~ igraph::graph_attr(g, .x)) %>%
-    rlang::set_names(ga_names)
-  g_full <- subgraph_full(g, vids)
-  g_intra <- vids %>%
-    purrr::map(~ igraph::induced_subgraph(g, .x)) %>%
-    purrr::map(delete_graph_attributes) %>%
-    purrr::reduce(igraph::disjoint_union) %>%
-    add_graph_attributes(ga_values) %>%
-    igraph::set_graph_attr("atoms", names(vids))
-  g_inter <- igraph::difference(g_full, g_intra)
-  list(full = g_full, intra = g_intra, inter = g_inter)
 }
 
 delete_graph_attributes <- function(g) {
